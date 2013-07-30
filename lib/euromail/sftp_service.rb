e301e0e1a7dbf5cb868a04ad982bd23184a7b15e
Stefan Teijgeler <stefan.teijgeler@nedap.com>
2013-07-30 09:40:42 +0200

    Refactored and added #upload and #remove to be called within a block

--- lib/euromail/sftp_service.rb
+++ lib/euromail/sftp_service.rb
@@ -22,28 +22,61 @@ module Euromail
       self.extend(Euromail::SFTPDevelopment)
     end
 
+    # Upload pdf data to a file on the remote sftp server. Must be called within a connect block:
+    # euromail.connect do |service|
+    #   service.upload('some-data', '1')
+    # end
+    def upload pdf_data, identifier
+      raise "Can only be called in a connect block" unless @sftp
+      @sftp.file.open( filename(identifier) , "w") do |f|
+        f.write pdf_data
+      end      
+    end
+
+    # Attempt to remove the file for the given identifier. If the upload fails or is aborted,
+    # this method attempts to remove the incomplete file from the remote server.
     def upload! pdf_data, identifier
       begin
-        connect do |sftp|
-          sftp.file.open( filename(identifier) , "w") do |f|
-            f.write pdf_data
-          end
+        connect do |service|
+          service.upload(pdf_data, identifier)
         end
       rescue => e
         remove!(identifier)
         raise e
       end
     end
+    
+    # Removes a pdf file on the remote sftp server. Must be called within a connect block:
+    # euromail.connect do |service|
+    #   service.remove('1')
+    # end
+    def remove identifier
+      raise "Can only be called in a connect block" unless @sftp
+      @sftp.remove!( filename(identifier) )
+    end
 
     # Attempt to remove the file for the given identifier. 
     def remove! identifier
-      connect do |sftp|
-        sftp.remove!( filename(identifier) )
+      connect do |service|
+        service.remove( identifier )
       end
     end
-    
+
+    # Setup a connection to the sftp server. Operations can be defined in the block passed to this method:
+    # euromail.connect do |service|
+    #   service.upload('some data', '1')
+    #   service.upload('more data', '2')
+    #   service.remove('3')
+    # end
     def connect &block
-      Net::SFTP.start(host, username, :password => password, &block)
+      Net::SFTP.start(host, username, :password => password) do |sftp|
+        begin
+          @sftp = sftp
+          block.call(self)
+        ensure
+          @sftp = nil
+        end
+      end
     end
 
     # Generate a filename based on the application, customer and some unique identifier.
--- spec/euromail/sftp_service_spec.rb
+++ spec/euromail/sftp_service_spec.rb
@@ -7,84 +7,134 @@ describe Euromail::SFTPService do
     mock_sftp
   end
 
-  let(:service) do
+  let(:euromail) do
     Euromail::SFTPService.new('moves', 'nedap', 'some-cheapass-domain.com', "stefan", "super_secret")
   end
 
   context "when creating" do
     it "has an application, customer, host, username and password" do
-      service.application.should eql('moves')
-      service.customer.should eql('nedap')
-      service.host.should eql('some-cheapass-domain.com')
-      service.username.should eql('stefan')
-      service.password.should eql('super_secret')
+      euromail.application.should eql('moves')
+      euromail.customer.should eql('nedap')
+      euromail.host.should eql('some-cheapass-domain.com')
+      euromail.username.should eql('stefan')
+      euromail.password.should eql('super_secret')
     end
   end
 
   describe "#connect" do
     it "connects to euromail using the given username and pass" do
       Net::SFTP.should receive(:start).with('some-cheapass-domain.com', 'stefan', :password => 'super_secret')
-      service.connect {}
+      euromail.connect {}
+    end
+    
+    it "unsets the connection if some error occurs" do
+      expect { 
+        euromail.connect do |service|
+          raise 'boem'
+        end
+      }.to raise_error
+
+      # This should raise an error if the internal sftp connection is cleared
+      expect{ euromail.remove('1') }.to raise_error(RuntimeError)
+    end
+    
+  end
+
+  describe "#upload" do
+    it "use the generated filename" do
+      @net_sftp_session.file.should receive(:open).with( euromail.filename('1'), 'w')
+      euromail.connect do |service|
+        service.upload('some-client-code', '1')
+      end
+    end
+
+    it "must be called within a connect block" do
+      expect{ euromail.upload('some-client-code', '1')}.to raise_error(RuntimeError)
+      expect{
+        euromail.connect do |service|
+          service.upload('some-client-code', '1')
+          service.upload('another-client-code', '2')
+        end
+      }.to_not raise_error
+    end
+
+    it "does not reestablish a connection for each upload" do
+      expect(Net::SFTP).to receive(:start).once
+      euromail.connect do |service|
+        service.upload('some-client-code', '1')
+        service.upload('another-client-code', '2')
+      end
+    end
+
+    it "can upload several pdf files within the same connection" do
+      expect(@net_sftp_session.file).to receive(:open).exactly(2).times
+      @file_hander.should receive(:write).with('some-client-code-1')
+      @file_hander.should receive(:write).with('some-client-code-2')
+
+      euromail.connect do |service|
+        service.upload("some-client-code-1", '1')
+        service.upload("some-client-code-2", '2')
+      end
     end
   end
 
   describe "#upload!" do
-    it "use the generated filename" do
-      @net_sftp_session.file.should receive(:open).with( service.filename('1'), 'w')
-      service.upload!('some-client-code', '1')
-    end
-
     it "uploads pdf data" do
       @file_hander.should receive(:write).with('some-client-code')
-      service.upload!('some-client-code', '1')
+      euromail.upload!('some-client-code', '1')
     end
 
     it "does not remove the remote file after an upload succeeds" do
-      service.should_not receive(:remove!).with('1')
-      service.upload!('some-client-code', '1')
+      euromail.should_not receive(:remove!).with('1')
+      euromail.upload!('some-client-code', '1')
     end
 
     it "tries to remove the remote file after an upload fails" do
       @file_hander.stub(:write).and_raise("Connection dropped")
-      service.should receive(:remove!).with('1')
-      expect{ service.upload!('some-client-code', '1') }.to raise_error
+      euromail.should receive(:remove!).with('1')
+      expect{ euromail.upload!('some-client-code', '1') }.to raise_error
     end
 
-    it "returns true if it succeeds" do
-      expect{ service.upload!('some-client-code', '2') }.to_not raise_error
-    end
-
-    it "return false if some error occurs" do
+    it "raises if some error occurs" do
       @net_sftp_session.stub_chain(:file, :open).and_raise("Some error")
-      expect{ service.upload!('some-client-code', '2') }.to raise_error
+      expect{ euromail.upload!('some-client-code', '2') }.to raise_error
+    end
+  end
+
+  describe "#remove" do
+    it "removes the file with the generated filename" do
+      @net_sftp_session.should receive(:remove!).with( euromail.filename('1') )
+      euromail.connect do |service|
+        service.remove('1')
+      end
+    end
+
+    it "must be called within a connect block" do
+      expect{ euromail.upload('some-client-code', '1')}.to raise_error(RuntimeError)
     end
   end
 
   describe "#remove!" do
     it "removes the file from the sftp server" do
-      @net_sftp_session.should receive(:remove!).with( service.filename('2') )
+      @net_sftp_session.should receive(:remove!).with( euromail.filename('2') )
       @net_sftp_session.file.should_not receive(:open)
-      service.remove!('2')
+      euromail.remove!('2')
     end
 
-    it "returns true if it succeeds" do
-      expect{ service.remove!('2') }.to_not raise_error
-    end
-
-    it "return false if some error occurs" do
+    it "raises if some error occurs" do
       @net_sftp_session.stub(:remove!).and_raise("Some error")
-      expect{ service.remove!('2') }.to raise_error
+      expect{ euromail.remove!('2') }.to raise_error
     end
   end
 
   describe "#filename" do 
     it "generates a string with application, customer and the given identifier" do
-      service.filename('123').should eql('./moves_nedap_123.pdf')
+      euromail.filename('123').should eql('./moves_nedap_123.pdf')
     end
 
     it "requires a non empty identifier" do
-      expect{ service.filename('') }.to raise_error
-      expect{ service.filename(nil) }.to raise_error
+      expect{ euromail.filename('') }.to raise_error
+      expect{ euromail.filename(nil) }.to raise_error
     end
   end
 
